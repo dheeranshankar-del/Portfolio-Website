9a9c68e4247417988ab4eb8c211ade75191cfd19
Dheeran Shankar <dheeranshankar@gmail.com>
2026-09-04 19:07:43 -0400

    Update PCB auto-highlight display duration to 1.75 seconds

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -31,7 +31,7 @@ function EditorialProjectSection({ project }) {
     setPcbAutoHighlight(true);
     setTimeout(() => {
       setPcbAutoHighlight(false);
-    }, 2500);
+    }, 1750); // Show PCB callout labels for 1.75 seconds
   };
 
   const isAlternate = project.layout === 'alternate';
